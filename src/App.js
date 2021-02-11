07a821448db56ba049d75d13a4c518e48bf57a6a
alexandr <santichechecheck@gmail.com>
2021-02-11 13:34:35 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useDropzone } from "react-dropzone";
 
 function App() {
+  const [name, setName] = useState('');
+  const onDrop = async (files) => {
+    await Promise.all(Array.from(files).map(f => {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const config = JSON.parse(event.target.result);
+
+        fetch('https://test.uebok.com/v1/lotties/add', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify({
+            name,
+            config
+          })
+        })
+      };
+
+      reader.readAsText(f);
+    }))
+  }
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: 'application/json'
+  });
+
+  const handleNameChange = ({ target }) => {
+    setName(target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={handleNameChange} placeholder="Name" />
+      <div {...getRootProps()}>
+        <input className="dropzone-input" {...getInputProps()} />
+        <div className="text-center">
+          {isDragActive ? (
+            <p className="dropzone-content">Release to drop the files here</p>
+          ) : (
+            <p className="dropzone-content">
+              Drag 'n' drop some files here, or click to select files
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
